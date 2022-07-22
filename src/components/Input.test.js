f4fa693fb09d2b0e8af6f8e372213c28bb7f8425
KurtReyn <kurtreyn@yahoo.com>
2022-07-22 15:53:16 -0400

    added more tests

--- src/components/Input.test.js
+++ src/components/Input.test.js
@@ -1,10 +1,31 @@
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import Input from './Input';
 
 describe('Input Component Test', () => {
   it('rendered input', () => {
-    const { getByTestId } = render(<Input showDiv={false} />);
-    const input = getByTestId('searchBar');
+    render(<Input showDiv={false} />);
+    const input = screen.getByTestId('searchBar');
     expect(input).toBeTruthy();
+  });
+
+  it('render div', () => {
+    render(<Input showDiv={true} />);
+    const div = screen.getByTestId('divWeWantToShow');
+    expect(div).toBeTruthy();
+  });
+
+  it(`don't render div`, () => {
+    render(<Input showDiv={false} />);
+    const div = screen.getByTestId('divWeWantToShow');
+    expect(div).toBeFalsy();
+  });
+
+  it('change on input causes change on header', async () => {
+    render(<Input showDiv={true} />);
+    const input = screen.getByTestId('searchBar');
+    const inputWord = 'Kurt';
+    fireEvent.change(input, { target: { value: inputWord } });
+    const header = await screen.findByTestId('displaySearch');
+    expect(header.value).toBe(inputWord);
   });
 });
